3af3793e0740433f5f4adfe145708ffe66e6e78d
Jonathan Célio <jonathan.clio@hotmail.com>
2021-06-13 19:20:56 -0300

    feat: first commit

---
+++ src/infra/AxiosHttpClient.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+export class AxiosHttpClient {
+  constructor(baseURL, headers) {
+    this.axios = axios.create({
+      baseURL,
+      headers,
+    });
+  }
+  get(url, params = {}) {
+    return this.axios.get(url, {
+      params,
+    });
+  }
+}
