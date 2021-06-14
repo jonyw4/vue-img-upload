6e370ab3b3b59dba441efbdce0dfbf66d3bd0954
Jonathan Célio <jonathan.clio@hotmail.com>
2021-06-14 18:05:41 -0300

    refactor: move base layout spec

--- tests/unit/view/organisms/BaseLayout.spec.js
+++ tests/unit/view/organisms/BaseLayout.spec.js
@@ -1,7 +1,7 @@
 import { mount } from "@vue/test-utils";
-import BaseLayout from "../../../../src/view/templates/BaseLayout.vue";
+import BaseLayout from "../../../../src/view/organisms/BaseLayout.vue";
 
-describe("view :: templates :: BaseLayout", () => {
+describe("view :: organisms :: BaseLayout", () => {
   const wrapper = mount(BaseLayout, {
     slots: {
       default: "<h1>Hello World</h1>",
